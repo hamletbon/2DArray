1642592b084291e60da4b5c42088afd3b2b6fd31
hamletbon <halin@hkis.edu.hk>
2016-11-17 14:05:57 +0800

    Init with .length

--- TwoDArrayTest.java
+++ TwoDArrayTest.java
@@ -15,25 +15,11 @@ public class TwoDArrayTest
      */
     public static void main()
     {
-       int[][] a2dArray = new int[3][4]; 
-       /*
-       a2dArray[0][0] = 1;
-       a2dArray[0][1] = 2;
-       a2dArray[0][2] = 3;
-       a2dArray[0][3] = 4;
-       a2dArray[1][0] = 5;
-       a2dArray[1][1] = 6;
-       a2dArray[1][2] = 7;
-       a2dArray[1][3] = 8;
-       a2dArray[2][0] = 9;
-       a2dArray[2][1] = 10;
-       a2dArray[2][2] = 11;
-       a2dArray[2][3] = 12;
-       */
-      int counter = 0;
-      for (int r = 0; r < 3; r++)
+       int[][] a2dArray = new int[200][100]; 
+      int counter = 1;
+      for (int r = 0; r < a2dArray.length; r++)
       {
-          for (int c = 0; c < 4; c++)
+          for (int c = 0; c < a2dArray[r].length; c++)
           {
               System.out.println("a2dArray[" + r + "][" + c + "]: "+counter);
               a2dArray[r][c] = counter;
