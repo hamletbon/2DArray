5528c4aae36e28c1c695c69e139145c0b529a4c4
hamletbon <halin@hkis.edu.hk>
2016-11-17 13:49:26 +0800

    init commit

---
+++ TwoDArrayTest.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class TwoDArrayTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TwoDArrayTest
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class TwoDArrayTest
+     */
+    public TwoDArrayTest()
+    {
+        int[][] a2dArray = new int[3][4]; 
+       a2dArray[0][0] = 1;
+       a2dArray[0][1] = 2;
+       a2dArray[0][2] = 3;
+       a2dArray[0][3] = 4;
+       a2dArray[1][0] = 5;
+       a2dArray[1][1] = 6;
+       a2dArray[1][2] = 7;
+       a2dArray[1][3] = 8;
+       a2dArray[2][0] = 9;
+       a2dArray[2][1] = 10;
+       a2dArray[2][2] = 11;
+       a2dArray[2][3] = 12;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
